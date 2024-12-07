d81ceb3f7c0ef818f286d50d2b82c2a083906fc9
Yanarsi <kultaevaana@gmail.com>
2024-12-07 19:14:28 +0300

    Перенос java script в отдельный файл

--- main.js
+++ main.js
@@ -19,7 +19,12 @@ function setupModal(openButtons, modal, closeButton) {
     }
   });
 }
+const toggleBtn = document.querySelector(".toggle_btn");
+const dropdownMenu = document.querySelector(".dropdown_menu");
 
+toggleBtn.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("open");
+});
 const registerButtons = document.querySelectorAll(
   ".register-button, .header-button"
 );
