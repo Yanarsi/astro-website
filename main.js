56757fbc0e96ebd66db3b4ea69a89567f35cc075
Yanarsi <kultaevaana@gmail.com>
2024-12-10 14:31:58 +0300

    Переименование страниц на английский

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Модпльное окно для формы
 function openModal(modal) {
   modal.style.display = "block";
 }
@@ -6,6 +7,7 @@ function closeModal(modal) {
   modal.style.display = "none";
 }
 
+// Функция для настройки модального окна
 function setupModal(openButtons, modal, closeButton) {
   openButtons.forEach((button) => {
     button.addEventListener("click", () => openModal(modal));
@@ -19,27 +21,33 @@ function setupModal(openButtons, modal, closeButton) {
     }
   });
 }
+
+// Выпадающее меню
 const toggleBtn = document.querySelector(".toggle_btn");
 const dropdownMenu = document.querySelector(".dropdown_menu");
 
 toggleBtn.addEventListener("click", () => {
   dropdownMenu.classList.toggle("open");
 });
+
+//Модальное окно бронирования
 const registerButtons = document.querySelectorAll(
   ".register-button, .header-button"
 );
-const bookingModal = document.getElementById("booking-modal");
+const bookingModal = document.getElementById("booking-modal"); // Модальное окно бронирования по ID
 const closeBookingModal = document.querySelector(".close-modal");
 
 setupModal(registerButtons, bookingModal, closeBookingModal);
 
-const imageItems = document.querySelectorAll(".image-item");
-const lightbox = document.querySelector(".lightbox");
-const lightboxImage = document.querySelector(".lightbox-image");
-const closeLightbox = document.querySelector(".close-lightbox");
+//Фотогалерея
+const imageItems = document.querySelectorAll(".image-item"); // Все элементы с изображениями
+const lightbox = document.querySelector(".lightbox"); // Лайтбокс
+const lightboxImage = document.querySelector(".lightbox-image"); // Изображение внутри лайтбокса
+const closeLightbox = document.querySelector(".close-lightbox"); // Кнопка закрытия лайтбокса
 
 imageItems.forEach((item) => {
   item.addEventListener("click", () => {
+    // Получаем путь к изображению из элемента
     const imgSrc = item.querySelector("img").src;
     lightboxImage.src = imgSrc;
     lightbox.style.display = "flex";
@@ -50,6 +58,7 @@ closeLightbox.addEventListener("click", () => {
   lightbox.style.display = "none";
 });
 
+// Добавляем обработчик события клика на фон лайтбокса
 lightbox.addEventListener("click", (event) => {
   if (event.target === lightbox) {
     lightbox.style.display = "none";
